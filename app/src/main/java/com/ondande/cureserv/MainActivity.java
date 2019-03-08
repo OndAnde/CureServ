af00e35b01d812664409f26b6bd160a43276ac24
OndAnde <pashamanulik@gmail.com>
2019-03-09 01:38:59 +0300

    facebook kit, keyhash, fragments

--- app/src/main/java/com/ondande/cureserv/MainActivity.java
+++ app/src/main/java/com/ondande/cureserv/MainActivity.java
@@ -1,7 +1,19 @@
 package com.ondande.cureserv;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        printKeyHash();
+    }
+
+    private void printKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.ondande.cureserv", PackageManager.GET_SIGNATURES);
+            for(Signature signature:info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KEYHASH", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void Change(View view){
+        Fragment fragment = null;
+
+        switch (view.getId()){
+            case R.id.btn_sign_in:
+                fragment = new sign_in();
+                break;
+            case R.id.btn_sign_up:
+                fragment = new sign_up();
+                break;
+        }
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        assert fragment != null;
+        ft.replace(R.id.fr_place, fragment);
+        ft.commit();
     }
 }
