53d4682247840d226496d866ae0bedd953609040
OndAnde <pashamanulik@gmail.com>
2019-03-20 16:47:49 +0300

    sign up, facebook validation

--- app/src/main/java/com/ondande/cureserv/MainActivity.java
+++ app/src/main/java/com/ondande/cureserv/MainActivity.java
@@ -1,28 +1,129 @@
 package com.ondande.cureserv;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
+import com.ondande.cureserv.Model.CheckUserResponse;
+import com.ondande.cureserv.Retrofit.CureServAPI;
+import com.ondande.cureserv.Utils.Common;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import dmax.dialog.SpotsDialog;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_sign_up;
+    private static final int REQUEST_CODE = 1000;
+    CureServAPI mService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mService = Common.getAPI();
+
+        btn_sign_up = (Button)findViewById(R.id.btn_sign_up);
+        btn_sign_up.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                 startLoginPage(LoginType.PHONE);
+            }
+        });
+
 //        printKeyHash();
+    }
+
+    private void startLoginPage(LoginType loginType) {
+
+        Intent intent = new Intent(this, AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder builder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(loginType,
+                        AccountKitActivity.ResponseType.TOKEN);
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, builder.build());
+        startActivityForResult(intent, REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE){
+            AccountKitLoginResult result = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+
+            if(result.getError() != null){
+                Toast.makeText(this, ""+result.getError().getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+            else if(result.wasCancelled()){
+                Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                if(result.getAccessToken() != null){
+                    final SpotsDialog alertDialog = new SpotsDialog(MainActivity.this);
+                    alertDialog.show();
+                    alertDialog.setMessage("Waiting...");
+
+                    AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                        @Override
+                        public void onSuccess(Account account) {
+                            mService.checkUserExists(account.getPhoneNumber().toString())
+                                    .enqueue(new Callback<CheckUserResponse>() {
+                                        @Override
+                                        public void onResponse(Call<CheckUserResponse> call, Response<CheckUserResponse> response) {
+                                            CheckUserResponse userResponse = response.body();
+                                            if(userResponse.isExists()){
+                                                alertDialog.dismiss();
+                                            }
+                                            else{
+                                                alertDialog.dismiss();
+
+                                                Change(2);
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onFailure(Call<CheckUserResponse> call, Throwable t) {
+
+                                        }
+                                    });
+                        }
+
+                        @Override
+                        public void onError(AccountKitError accountKitError) {
+                            Log.d("ERROR", accountKitError.getErrorType().getMessage());
+                        }
+                    });
+                }
+            }
+        }
     }
 
     private void printKeyHash() {
@@ -41,14 +142,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void Change(View view){
+    public void Change(int val){
         Fragment fragment = null;
 
-        switch (view.getId()){
-            case R.id.btn_sign_in:
+        switch (val){
+            case 1:
                 fragment = new sign_in();
                 break;
-            case R.id.btn_sign_up:
+            case 2:
                 fragment = new sign_up();
                 break;
         }
--- app/src/main/java/com/ondande/cureserv/sign_up.java
+++ app/src/main/java/com/ondande/cureserv/sign_up.java
@@ -8,13 +8,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.rengwuxian.materialedittext.MaterialEditText;
+
 public class sign_up extends Fragment {
+    MaterialEditText edt_name;
+    MaterialEditText edt_address;
+    MaterialEditText edt_birth;
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        edt_name = (MaterialEditText)view.findViewById(R.id.edt_name);
+        edt_address = (MaterialEditText)view.findViewById(R.id.edt_address);
+        edt_birth = (MaterialEditText)view.findViewById(R.id.edt_birth);
+        return view;
+    }
+
+    public void Registration(String phone){
+        
     }
 
 }
